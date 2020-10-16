945dcca18489685fe28a78c1765bed13df56e3d4
Eric Lopes Oliveira <65664969+olvericc@users.noreply.github.com>
2020-10-16 11:22:39 -0300

    upload dia 16/10/2020

---
+++ calc.php
@@ -0,0 +1,28 @@
+<?php
+    
+    $altura = $_POST["txtAltura"];
+    $peso = $_POST["txtPeso"];
+    $calculo = $peso/($altura*$altura);
+    $resp = "";
+
+    if ($calculo <= 18.5){
+        $resp = "Abaixo do Peso";   
+    }else if ($calculo > 18.5 && $calculo < 25){
+        $resp = "Saudável";
+    }else if ($calculo >= 25 && $calculo < 30){
+        $resp = "Peso em Excesso";
+    }else if ($calculo >= 30 && $calculo < 35){
+        $resp = "Obesidade Grau 1";
+    }else if ($calculo >= 35 && $calculo < 40){
+        $resp = "Obesidade Grau 2";
+    }else if ($calculo >= 40){
+        $resp = "Obsidade Grau 3";
+    }else{
+        $resp = "IMC fora do Intervalo!";
+    } 
+
+    echo ("IMC: ".$calculo);
+    echo "<br>";
+    echo ("Resultado: $resp");
+
+?>
